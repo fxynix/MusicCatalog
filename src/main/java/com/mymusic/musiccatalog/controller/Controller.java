5d41472982f08625fee8c223480ddfd4cb0624b3
fxynix <veremeyanton@yandex.ru>
2025-03-02 19:22:21 +0300

    added controller

---
+++ src/main/java/com/mymusic/musiccatalog/controller/Controller.java
@@ -0,0 +1,35 @@
+package com.mymusic.musiccatalog.controller;
+
+import java.util.List;
+import com.mymusic.musiccatalog.service.MusicService;
+import com.mymusic.musiccatalog.Music;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/music")
+public class Controller {
+    private final MusicService musicService;
+
+    @Autowired
+    public Controller(MusicService musicService) {
+        this.musicService = musicService;
+    }
+
+    @GetMapping
+    public Music getMusicById(@RequestParam int id) {
+        return musicService.getMusicById(id);
+    }
+
+    @GetMapping("/{name}")
+    public List<Music> getMusicByName(@PathVariable String name) {
+        return musicService.getMusicByName(name);
+    }
+
+
+
+}
