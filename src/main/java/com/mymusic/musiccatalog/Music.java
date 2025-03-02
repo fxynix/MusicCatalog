1f08cde4840135d74830ed0f330b261ac14eeac4
fxynix <veremeyanton@yandex.ru>
2025-03-02 17:48:32 +0300

    pom fix & new class

---
+++ src/main/java/com/mymusic/musiccatalog/Music.java
@@ -0,0 +1,23 @@
+package com.mymusic.musiccatalog;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Music {
+    private int id;
+    private String name;
+    private String author;
+    private String album;
+    private int duration;
+
+}
---
+++ src/test/java/com/mymusic/musiccatalog/MusicCatalogApplicationTest.java
@@ -0,0 +1,13 @@
+package com.mymusic.musiccatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicCatalogApplicationTests {
+
+	@Test
+	void contextLoads()	{	// test
+	}
+
+}
