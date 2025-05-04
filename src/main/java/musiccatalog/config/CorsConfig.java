338e165da1b090aa7b5e2257cc118e7214bf5d79
fxynix <veremeyanton@yandex.ru>
2025-05-04 16:38:44 +0300

    ui fix

--- src/main/java/musiccatalog/config/CorsConfig.java
+++ src/main/java/musiccatalog/config/CorsConfig.java
@@ -1,6 +1,5 @@
 package musiccatalog.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,16 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-    @Value("${cors.allowed.origins:http://localhost,http://localhost:80,http://localhost:3000,https://music-catalog-frontend.onrender.com}")
-    private String allowedOrigins;
+
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins(allowedOrigins.split(","))
+                .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowCredentials(true)
-                .maxAge(3600);
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH");
     }
 }
