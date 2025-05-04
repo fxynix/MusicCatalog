8ac3664f1882a8297e0b683772d4d26f937b0356
fxynix <veremeyanton@yandex.ru>
2025-05-04 18:15:43 +0300

    fix

--- src/main/java/musiccatalog/MusicCatalogApplication.java
+++ src/main/java/musiccatalog/MusicCatalogApplication.java
@@ -1,13 +1,20 @@
 package musiccatalog;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicCatalogApplication {
-
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure()
+				.filename(".env.development")
+				.directory(System.getProperty("user.dir"))
+				.ignoreIfMissing()
+				.load();
+
+		dotenv.entries().forEach(e -> System.setProperty(e.getKey(), e.getValue()));
+
 		SpringApplication.run(MusicCatalogApplication.class, args);
 	}
-
 }
