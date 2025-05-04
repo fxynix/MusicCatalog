c0940383ff979997443b808a4661de383dab4ccc
fxynix <veremeyanton@yandex.ru>
2025-05-04 06:01:39 +0300

    fix

--- src/main/java/musiccatalog/config/CorsConfig.java
+++ src/main/java/musiccatalog/config/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // React порт
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
                 .allowCredentials(true);
     }
